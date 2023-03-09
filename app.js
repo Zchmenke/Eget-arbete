abbd5ba4a94a6e1e6af1bee9bb120446f8cddc9d
Henrik Rydén <henrik.ryden@hotmail.com>
2023-03-09 19:27:01 +0100

    Cleaned my json file and updated Js to match,

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 
  // title and educations.
 const education = document.querySelector("h2.education")
-const edu1 = document.querySelector("li.edu1")
-const edu2 = document.querySelector("li.edu2")
-const edu3 = document.querySelector("li.edu3")
+let edu1 = document.querySelector("li.edu1")
+let edu2 = document.querySelector("li.edu2")
+let edu3 = document.querySelector("li.edu3")
 
 
 // title and workplaces.
 const workTitle = document.querySelector("h2.workTitle")
-const work1 = document.querySelector("li.work1")
-const work2 = document.querySelector("li.work2")
-const work3 = document.querySelector("li.work3")
-const work4 = document.querySelector("li.work4")
+let work1 = document.querySelector("li.work1")
+let work2 = document.querySelector("li.work2")
+let work3 = document.querySelector("li.work3")
+let work4 = document.querySelector("li.work4")
 
 //function to print my CV to site.
 const printResume = () => {
@@ -19,21 +19,23 @@ const printResume = () => {
     fetch('/cv.json')
      .then(res=>res.json())
      .then (response=> {
-
+ console.log(response)
         // edu
-        education.innerText=response.experience.title;
-        edu1.innerText=response.experience.estet;
-        edu2.innerText=response.experience.folkis;
-        edu3.innerText=response.experience.bth;
+        education.innerText=response.education[0].title;
+        edu1.innerText=response.education[1].school;
+        edu2.innerText=response.education[2].school;
+        edu3.innerText=response.education[3].university;
        // work
-        workTitle.innerText=response.experience.workTitle;
-        work1.innerText=response.experience.demo;
-        work2.innerText=response.experience.kosmo;
-        work3.innerText=response.experience.derome;
+        workTitle.innerText=response.workExperience[0].title;
+        work1.innerText=response.workExperience[1].employer1;
+        work2.innerText=response.workExperience[2].employer2;
+        work3.innerText=response.workExperience[3].employer3;      
     })
 }
-// call function
+//call function
 printResume()
+
+
 
 
 
